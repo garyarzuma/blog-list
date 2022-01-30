a1723dacd6b9a05c0a00559a40a5855e20d24a38
garyarzumanyan <garyarzumanyan@gmail.com>
2022-01-29 16:42:53 -0800

    Finished ex 4.11

--- tests/blogRoutes.test.js
+++ tests/blogRoutes.test.js
@@ -55,9 +55,23 @@ describe('when there is initially some blogs saved', () => {
         .expect('Content-Type', /application\/json/)
       const endLength = await helper.blogsInDb()
       expect(endLength.length).toBe(curLength.length+1)
-      })    
+      }) 
+      
+    test('blog added without likes defaults to 0', async () => {
+      const newBlog = {
+        title: "A Bad Blog",
+        author: "Gary Arzumanyan",
+        url: "https://badBlogs.com/",
+      }
+      const response = await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+      
+      expect(response.body.likes).toBe(0)
   })
-
+})
 afterAll(() => {
   mongoose.connection.close()
 })
