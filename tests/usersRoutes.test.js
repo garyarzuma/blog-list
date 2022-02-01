5a659f7517b033cb124d630c03559cb3b55bee0f
garyarzumanyan <garyarzumanyan@gmail.com>
2022-01-31 22:53:31 -0800

    Added tests for checking password req. Finished Ex 4.15 and 4.16

--- tests/usersRoutes.test.js
+++ tests/usersRoutes.test.js
@@ -59,6 +59,48 @@ describe('when there is initially one user in db', () => {
         const usersAtEnd = await helper.usersInDb()
         expect(usersAtEnd).toHaveLength(usersAtStart.length)
       })
+
+      test('creation fails with proper statuscode and message if username is too short', async () => {
+        const usersAtStart = await helper.usersInDb()
+    
+        const newUser = {
+          username: 'ro',
+          name: 'Superuser',
+          password: 'salainen',
+        }
+    
+        const result = await api
+          .post('/api/users')
+          .send(newUser)
+          .expect(400)
+          .expect('Content-Type', /application\/json/)
+    
+        expect(result.body.error).toContain('is shorter')
+    
+        const usersAtEnd = await helper.usersInDb()
+        expect(usersAtEnd).toHaveLength(usersAtStart.length)
+      })
+
+      test('creation fails with proper statuscode and message if password is too short', async () => {
+        const usersAtStart = await helper.usersInDb()
+    
+        const newUser = {
+          username: 'root',
+          name: 'Superuser',
+          password: 'sn',
+        }
+    
+        const result = await api
+          .post('/api/users')
+          .send(newUser)
+          .expect(400)
+          .expect('Content-Type', /application\/json/)
+    
+        expect(result.body.error).toContain('Password too short')
+    
+        const usersAtEnd = await helper.usersInDb()
+        expect(usersAtEnd).toHaveLength(usersAtStart.length)
+      })
 })
 
 afterAll(() => {
