b555ce96516b9772c0e698306e1013f4021367d0
garyarzumanyan <garyarzumanyan@gmail.com>
2022-01-31 21:39:35 -0800

    Added routes to add blog with a user

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,5 +1,6 @@
 const blogRouter = require('express').Router()
 const Blog = require("../models/blog")
+const User = require('../models/user')
 
 blogRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({})
@@ -7,10 +8,22 @@ blogRouter.get('/', async (request, response) => {
 })
   
 blogRouter.post('/', async (request, response) => {
-    const blog = new Blog(request.body)
+    const body = request.body
+    const user = await User.findById(body.userId)
+
+    const blog = new Blog({
+      title: body.title,
+      author: body.author,
+      url:  body.url,
+      likes: body.likes,
+      user: user._id
+    })
+
     const result = await blog.save()
+    user.blogs = user.blogs.concat(result._id)
+    await user.save()
     response.status(201).json(result)
-  })
+})
 
 blogRouter.delete('/:id', async (request, response) => {
   await Blog.findByIdAndRemove(request.params.id)
